18ed009ebbc69af0f43b3e9a56d85b435a919620
AndreasVFromAU <201705136@post.au.dk>
2021-10-12 20:11:48 +0200

    Draft for REST API

---
+++ REST_API/REST_API/Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+        private readonly ILogger<DataController> _logger;
+
+        public DataController(ILogger<DataController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
---
+++ REST_API/REST_API/MQTT/TemperatureSubscriber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace REST_API.MQTT
+{
+    public class TemperatureSubscriber
+    {
+        private const string MqttUri = "broker.mqttdashboard.com";
+        public static void Subscribe()
+        {
+            var client = new MqttClient(MqttUri);
+            client.MqttMsgPublishReceived += HandlePublishedMessage;
+            var clientId = Guid.NewGuid().ToString();
+
+            client.Connect(clientId);
+
+            client.Subscribe(new[] { "iot_grp10/temp" }, new[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+
+        static void HandlePublishedMessage(object sender, MqttMsgPublishEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Message);
+            // handle message received 
+        }
+    }
+}
