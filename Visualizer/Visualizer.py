9e6232b0f023182b933d9b268cbf0e1183db636c
Simon Jensen <simonzhjensen@gmail.com>
2021-11-03 13:27:27 +0100

    visualize this :eggplant: :sweat_drops: :peach:

--- Visualizer/Visualizer.py
+++ Visualizer/Visualizer.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import requests
 
 
-url = ''
+url = 'https://localhost:44340/'
 
 endpoints = {
-    '_accReading' : '',
-    '_airReading' : '',
-    '_humReading' : '',
-    '_tmpReading' : ''
+    '_accReading' : 'api/',
+    '_airReading' : 'api/',
+    '_humReading' : 'api/Humidity/Readings',
+    '_tmpReading' : 'api/'
 }
 
 
@@ -41,17 +42,65 @@ def set_reading_type():
     return type_of_reading
 
 
+def get_reading(input):
+    response = requests.get(url + endpoints['_humReading'], verify=False)
+    print(response.status_code)
+    print(response.json())
+    # return response.json()
 
-#def plot_readings(data):
+    dummy_data = [
+        {
+            "id": 0,
+            "timestamp": "2021-11-03T12:11:42.501Z",
+            "humidity": 0,
+            "sensorId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        },
+        {
+            "id": 0,
+            "timestamp": "2021-11-03T12:12:42.501Z",
+            "humidity": 1,
+            "sensorId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        },
+        {
+            "id": 0,
+            "timestamp": "2021-11-03T12:13:42.501Z",
+            "humidity": 2,
+            "sensorId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        },
+    ]
+    return dummy_data
 
 
+def convert_humidity_dataformat(data):
+    print(data)
+    humidity = []
+    dates = []
+    
+    for i in data:
+        humidity.append(i["humidity"])
+        print(humidity)
+
+        date = matplotlib.dates.date2num(i["timestamp"])
+        dates.append(date)
+
+    return humidity, dates
 
 
+def plot_reading(humidity, dates):
 
+    plt.plot_date(dates, humidity)
+    plt.xlabel("Time")
+    plt.xticks(rotation=10)
+    plt.ylabel("Humidity")
+    plt.show()
 
 
 def main():
-    set_reading_type()
+    #set_reading_type()
+    data = get_reading('tis')
+    humidity, dates = convert_humidity_dataformat(data)
+    plot_reading(humidity, dates)
+
     return 0
 
 
